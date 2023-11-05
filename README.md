5b44d7683f8b740ba41d12103bff9b3aa706236e
Daud Dhiya <daud.dhiya31@gmail.com>
2023-11-06 01:05:19 +0700

    bismillah

---
+++ README.md
@@ -0,0 +1,44 @@
+# Riva Vote
+
+## Introduction
+
+This project includes a bot implemented in JavaScript, specifically in a file named bot.js.
+
+## Requirements
+
+- Node.js
+- npm (comes with Node.js)
+- Any additional libraries or dependencies used in bot.js
+
+## Installation & Running the bot
+
+1. Clone this repository to your local machine.
+
+   ```git
+   git clone https://github.com/daudhiyaa/riva-vote.git
+   ```
+
+2. Navigate to the project directory.
+
+   ```
+   cd to/your/project/directory
+   ```
+
+3. Install the required dependencies
+
+   ```
+   npm i puppeteer
+   ```
+
+4. Run the bot
+   ```
+   node bot.js
+   ```
+
+## Troubleshooting
+
+If you encounter any issues while running the bot, ensure that:
+
+- You have the correct version of Node.js and npm installed.
+- All the required dependencies are installed.
+- There are no syntax errors in the bot.js file.
---
+++ bot.js
@@ -0,0 +1,180 @@
+const puppeteer = require("puppeteer"); // v20.7.4 or later
+
+const konfigbrowser = {
+  headless: false,
+  args: [
+    "--log-level=3", // fatal only
+
+    "--no-default-browser-check",
+    "--disable-infobars",
+    "--disable-web-security",
+    "--disable-site-isolation-trials",
+    "--no-experiments",
+    "--ignore-gpu-blacklist",
+    "--ignore-certificate-errors",
+    "--ignore-certificate-errors-spki-list",
+    "--mute-audio",
+    "--disable-extensions",
+    "--no-sandbox",
+  ],
+  disablejavascript: true,
+  ignoreHTTPSErrors: true,
+  incognito: true,
+  disablegpu: true,
+};
+
+const banyakVote = 10;
+
+(async () => {
+  const browser = await puppeteer.launch(konfigbrowser);
+  for (let i = 0; i < banyakVote; i++) {
+    const page = await browser.newPage();
+    const timeout = 5000;
+    page.setDefaultTimeout(timeout);
+
+    {
+      const targetPage = page;
+      await targetPage.setViewport({
+        width: targetPage.viewport().width,
+        height: 749,
+      });
+    }
+    {
+      const targetPage = page;
+      const promises = [];
+      const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
+      };
+      startWaitingForEvents();
+      await targetPage.goto(
+        "https://docs.google.com/forms/d/e/1FAIpQLScLoTY1JhXaDpdFamwEnMMTZ09EFKF7Gg_p3G2Vhpv0o9E-Mg/viewform"
+      );
+      await Promise.all(promises);
+    }
+    {
+      const targetPage = page;
+      await puppeteer.Locator.race([
+        targetPage.locator(
+          "div.o3Dpx > div:nth-of-type(1) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+        targetPage.locator('::-p-xpath(//*[@id=\\"i5\\"]/div[3]/div)'),
+        targetPage.locator(
+          ":scope >>> div.o3Dpx > div:nth-of-type(1) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+      ])
+        .setTimeout(timeout)
+        .click({
+          offset: {
+            x: 10,
+            y: 7.015625,
+          },
+        });
+    }
+    {
+      const targetPage = page;
+      await puppeteer.Locator.race([
+        targetPage.locator(
+          "div.o3Dpx > div:nth-of-type(2) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+        targetPage.locator('::-p-xpath(//*[@id=\\"i27\\"]/div[3]/div)'),
+        targetPage.locator(
+          ":scope >>> div.o3Dpx > div:nth-of-type(2) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+      ])
+        .setTimeout(timeout)
+        .click({
+          offset: {
+            x: 8,
+            y: 5.015625,
+          },
+        });
+    }
+    {
+      const targetPage = page;
+      await puppeteer.Locator.race([
+        targetPage.locator(
+          "div:nth-of-type(3) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+        targetPage.locator('::-p-xpath(//*[@id=\\"i40\\"]/div[3]/div)'),
+        targetPage.locator(
+          ":scope >>> div:nth-of-type(3) span > div > div:nth-of-type(1) div.vd3tt > div"
+        ),
+      ])
+        .setTimeout(timeout)
+        .click({
+          offset: {
+            x: 5,
+            y: 1.015625,
+          },
+        });
+    }
+    {
+      const targetPage = page;
+      await puppeteer.Locator.race([
+        targetPage.locator(
+          "div:nth-of-type(4) span > div > div:nth-of-type(4) div.vd3tt > div"
+        ),
+        targetPage.locator('::-p-xpath(//*[@id=\\"i71\\"]/div[3]/div)'),
+        targetPage.locator(
+          ":scope >>> div:nth-of-type(4) span > div > div:nth-of-type(4) div.vd3tt > div"
+        ),
+      ])
+        .setTimeout(timeout)
+        .click({
+          offset: {
+            x: 13,
+            y: 14.015625,
+          },
+        });
+    }
+    {
+      const targetPage = page;
+      await puppeteer.Locator.race([
+        targetPage.locator(
+          "div:nth-of-type(5) div:nth-of-type(4) div.vd3tt > div"
+        ),
+        targetPage.locator('::-p-xpath(//*[@id=\\"i90\\"]/div[3]/div)'),
+        targetPage.locator(
+          ":scope >>> div:nth-of-type(5) div:nth-of-type(4) div.vd3tt > div"
+        ),
+      ])
+        .setTimeout(timeout)
+        .click({
+          offset: {
+            x: 10,
+            y: 4.015625,
+          },
+        });
+    }
+    {
+      const targetPage = page;
+      const promises = [];
+      const startWaitingForEvents = () => {
+        promises.push(targetPage.waitForNavigation());
+      };
+      await puppeteer.Locator.race([
+        targetPage.locator("div.lRwqcd span > span"),
+        targetPage.locator(
+          '::-p-xpath(//*[@id=\\"mG61Hd\\"]/div[2]/div/div[3]/div/div[1]/div/span/span)'
+        ),
+        targetPage.locator(":scope >>> div.lRwqcd span > span"),
+        targetPage.locator("::-p-text(Kirim)"),
+      ])
+        .setTimeout(timeout)
+        .on("action", () => startWaitingForEvents())
+        .click({
+          offset: {
+            x: 31,
+            y: 18.015625,
+          },
+        });
+      await Promise.all(promises);
+    }
+
+    await page.close();
+  }
+  await browser.close();
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
